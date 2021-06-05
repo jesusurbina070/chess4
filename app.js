c7232fb11e631ca772b8e3991d764cf6a30058f3
Jesus Urbina <jesusrug2002@gmail.com>
2021-06-05 14:43:38 -0430

    creamos la clase Player

---
+++ app.js
@@ -0,0 +1,8 @@
+class Player {
+  constructor({ nombre, victorias, derrotas, puntos }) {
+    this.nombre = nombre;
+    this.victorias = victorias;
+    this.derrotas = derrotas;
+    this.puntos = puntos;
+  }
+}
