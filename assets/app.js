4bca4244bb84ab99e1c81879aadf351d51fce4ec
Jesus Urbina <jesusrug2002@gmail.com>
2021-06-08 21:08:09 -0430

    se agregaron los botones para avanzar y retroceder en el tiempo y entre otras muchas cosas

--- assets/app.js
+++ assets/app.js
@@ -2,6 +2,7 @@ class Player {
   constructor({ nombre, victorias, derrotas, puntos }) {
     this.nombre = nombre;
     this.victorias = victorias;
+    this.movimientos = 0;
     this.derrotas = derrotas;
     this.puntos = puntos;
     this.isAlive = true;
@@ -21,7 +22,6 @@ class Player {
 class Partida {
   constructor() {
     this.players = [];
-    this.movidas = 0;
     this.start = false;
     this.turno = 0;
   }
@@ -31,9 +31,13 @@ class Partida {
       this.players.forEach((elemento, index) => {
         if (p == index) {
           e.textContent = elemento.nombre;
-          elegirColores(index, e);
-          if (elemento.puntos > 0) {
-            e.textContent = `${elemento.puntos} pts`;
+          if (elemento.isAlive) {
+            elegirColores(index, e);
+            if (elemento.puntos > 0) {
+              e.textContent = `${elemento.puntos} pts`;
+            }
+          } else {
+            e.style.backgroundColor = colores.default;
           }
         }
       });
@@ -57,22 +61,39 @@ class Partida {
   }
 }
 
-const reglas = {
-  paso: 0,
-  peon: 1,
-  caballo: 3,
-  arfil: 5,
-  torre: 5,
-  dama: 9,
-  rey: 20,
-  jaqueDoble: 5,
-  jaqueTriple: 20,
-};
+class registroPartida {
+  constructor(jugador, puntos, kill = null) {
+    (this.jugador = jugador), (this.puntos = puntos), (this.kill = kill);
+  }
+  retroceder() {
+    partida.players[this.jugador].puntos -= this.puntos;
+    partida.turno = this.jugador;
+    if (this.kill != null) {
+      partida.players[this.kill].isAlive = true;
+    }
+    partida.mostrarPuntos();
+    $piezas.forEach(($pieza) => {
+      elegirColores(this.jugador, $pieza);
+    });
+  }
+  avanzar() {
+    partida.players[this.jugador].puntos += this.puntos;
+    partida.turno = this.jugador + 1;
+    if (this.kill != null) {
+      partida.players[this.kill].isAlive = false;
+    }
+    partida.mostrarPuntos();
+    $piezas.forEach(($pieza) => {
+      elegirColores(this.jugador + 1, $pieza);
+    });
+  }
+}
 const colores = {
   rojo: "#bf3b43",
   azul: "#4185bf",
   amarillo: "#c09526",
   verde: "#4e9161",
+  default: "#272522",
 };
 const $botones = document.querySelector(".hero");
 const $jugadores = document.querySelector(".jugadores");
@@ -83,16 +104,23 @@ const $overlay = document.querySelector(".overlay");
 let $jaqueMate;
 let $king = document.querySelector(".fa-chess-king");
 
+const $back = document.getElementById("back");
+const $moveContainer = document.querySelector(".move");
+const $move = document.querySelectorAll(".arrow");
+const $winner = document.querySelector(".fa-crown");
+
 const $botonPartida = document.getElementById("partida");
 const $botonJugadores = document.getElementById("jugadores");
 const $botonCrear = document.getElementById("crear");
-const $back = document.getElementById("back");
+
 const $back2 = document.querySelector(".back-king");
 const $start = document.querySelector(".start");
 
 const $lista = document.querySelector(".list");
 
 let partida;
+let registro = [];
+let i;
 const jugadores = [];
 
 $botonJugadores.addEventListener("click", () => {
@@ -150,12 +178,19 @@ $back2.addEventListener("click", () => {
 });
 $start.addEventListener("click", startMatch);
 $piezas.forEach(($pieza) => {
-  $pieza.addEventListener("click", () => {
-    if (!$pieza.classList.contains("fa-chess-king")) nextOne($pieza);
-  });
+  agregarEscuchador($pieza);
 });
-$king.addEventListener("click", () => {
-  partida.deadKing();
+
+$move.forEach(($boton) => {
+  if ($boton.classList.contains("fa-backward")) {
+    $boton.addEventListener("click", regresar);
+  }
+  if ($boton.classList.contains("fa-forward")) {
+    $boton.addEventListener("click", avanzar);
+  }
+  if ($boton.classList.contains("fa-play")) {
+    // $boton.addEventListener();
+  }
 });
 
 fetch("assets/server.json")
@@ -199,13 +234,18 @@ function getBack() {
   $start.classList.remove("is-active");
   partida.players = [];
   if (partida.start == true) {
+    $moveContainer.classList.add("none");
+    $winner.classList.add("none");
     $partida.classList.add("none");
     $partida.turno = 0;
+    registro = [];
     partida.start = false;
     reiniciarPuntos();
   }
 }
 function startMatch() {
+  $moveContainer.classList.remove("none");
+  $winner.classList.remove("none");
   removerAdder($partida, $jugadores);
   partida.start = true;
   ramdomArray(partida.players);
@@ -259,11 +299,17 @@ function reiniciarPuntos() {
 
 function jaqueMate() {
   $jaqueMate = document.querySelectorAll(".name");
-  $jaqueMate.forEach(($jaqueMate) => {
+
+  $jaqueMate.forEach(($jaqueMate, p) => {
     let nombre = $jaqueMate.textContent;
+    elegirColores(p, $jaqueMate);
     $jaqueMate.addEventListener("click", () => {
-      partida.players.forEach((e) => {
+      partida.players.forEach((e, p) => {
         if (nombre == e.nombre) {
+          registro.push(
+            new registroPartida(partida.turno, parseInt($king.dataset.price), p)
+          );
+          i = registro.length;
           e.isAlive = false;
           $overlay.classList.remove("is-active");
           hideName();
@@ -287,4 +333,30 @@ function nextOne(element) {
   partida.aumentarPuntos(element);
   partida.mostrarPuntos();
   cambiarTurno();
+}
+function agregarEscuchador($pieza) {
+  $pieza.addEventListener("click", () => {
+    if ($pieza.classList.contains("fa-chess-king")) {
+      partida.deadKing();
+    } else {
+      registro.push(
+        new registroPartida(partida.turno, parseInt($pieza.dataset.price))
+      );
+      i = registro.length;
+      nextOne($pieza);
+    }
+  });
+}
+function removerEscuchador($pieza) {}
+function regresar() {
+  if (i >= 1) {
+    registro[1 - i].retroceder();
+    i = i - 1;
+  }
+}
+function avanzar() {
+  if (i < registro.length) {
+    i = i + 1;
+    registro[1 - i].avanzar();
+  }
 }
